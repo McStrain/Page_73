b8c74d57fb4318423a8e2634c356a2d025c7df5c
McStrain <cbrandst@gmail.com>
2015-02-16 14:33:28 -0600

    1

---
+++ Page_73/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Page_73
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // this is a comment
+            string name = "Quentin";
+            int x = 3;
+            x = x * 17;
+            double d = Math.PI / 2;
+            myLabel.Text = "name is " + name + "\nx is " + x + "\nd is " + d;
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
