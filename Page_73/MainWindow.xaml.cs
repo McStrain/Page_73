9a34ebc5880f51ebaa1f049b926b0bff1afe36b8
McStrain <cbrandst@gmail.com>
2015-02-16 23:28:20 -0600

    Page 83, top of page

--- Page_73/MainWindow.xaml.cs
+++ Page_73/MainWindow.xaml.cs
@@ -72,7 +72,29 @@ namespace Page_73
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            myLabel.Text = "Testing...";
+            string result = "";
+            int x = 3;
+            while (x > 0)
+            {
+                if (x > 2) {
+                    result = result + "a";
+                }
+                if (x == 2)
+                {
+                    result = result + "b c";
+                }
+                if (x == 1)
+                {
+                    result = result + "d";
+                    x = x - 1;
+                }
+                x = x - 1;
+                if (x > 0)
+                {
+                    result = result + "-";
+                }
+            }
+            myLabel.Text = result;
         }
 
 
